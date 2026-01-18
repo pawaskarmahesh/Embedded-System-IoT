87dd98f1a901cae47cf398dee218b2d6055ece3b
pawaskarmahesh <131243670+pawaskarmahesh@users.noreply.github.com>
2026-01-18 21:54:21 +0530

    Add files via upload

---
+++ _01_Serial_out/_01_Serial_out.ino
@@ -0,0 +1,11 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+   Serial.print("Hello World ");
+   Serial.println("This is first Ptogram");
+   Serial.print("Lest's learn Electronics Circuits");
+   Serial.println(", Arduino ");
+   delay(5000);
+}
