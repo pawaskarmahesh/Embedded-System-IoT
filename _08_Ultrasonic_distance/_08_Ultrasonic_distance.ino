f080f6339ba4663414bb4503353b3bcdf88c8cfb
pawaskarmahesh <131243670+pawaskarmahesh@users.noreply.github.com>
2026-01-20 09:49:49 +0530

    Refactor ultrasonic sensor code to remove LCD
    Removed LCD display functionality and updated serial output message.

--- _08_Ultrasonic_distance/_08_Ultrasonic_distance.ino
+++ _08_Ultrasonic_distance/_08_Ultrasonic_distance.ino
@@ -1,20 +1,10 @@
-#include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
-
-LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
-
 #define trigger 4
 #define ecco 3
 
 void setup() {
-  lcd.init();                      // initialize the lcd 
-  lcd.init();
-  lcd.backlight();
   pinMode(trigger, OUTPUT);
   pinMode(ecco, INPUT);
   Serial.begin(9600);       
-  lcd.setCursor(0,0);
-  lcd.print("  Water level=  ");
 }
 
 void loop() {
@@ -26,18 +16,10 @@ void loop() {
   digitalWrite(trigger, LOW);
   duration = pulseIn(ecco, HIGH);
   distance = 0.034*duration/2;
-  
-  
-  Serial.print("Object is at ");
+  Serial.print("Distance between object and Sensor = ");
   Serial.print(distance);
   Serial.println(" cm");
-  
-
-  lcd.setCursor(5,1);
-  lcd.print("                ");
-  lcd.setCursor(5,1);
-  lcd.print(150- distance);
-  
-  delay(1500); 
+  delay(2000); 
 
 }
+
